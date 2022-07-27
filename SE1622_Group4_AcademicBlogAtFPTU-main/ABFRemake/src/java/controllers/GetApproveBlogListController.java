13b04c1246591293b99e49177af03ec11624bb14
ThanhHTSE150412 <105283138+ThanhHTSE150412@users.noreply.github.com>
2022-07-27 14:07:47 +0700

    New

---
+++ SE1622_Group4_AcademicBlogAtFPTU-main/ABFRemake/src/java/controllers/GetApproveBlogListController.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import dao.BlogDAO;
+import dto.BlogDTO;
+import dto.UserDTO;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author To Quyen Phan
+ */
+@WebServlet(name = "GetApproveBlogListController", urlPatterns = {"/GetApproveBlogListController"})
+public class GetApproveBlogListController extends HttpServlet {
+    private static final String ERROR = "error.jsp";
+    private static final String SUCCESS = "approveblog.jsp";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR;
+        try {
+            BlogDAO dao = new BlogDAO();
+            HttpSession session = request.getSession();
+            UserDTO loginUser = (UserDTO)session.getAttribute("LOGIN_USER");
+            int userID = loginUser.getUserID();
+            List<BlogDTO> listApproveBlogs = dao.getAllApproveBlogs(userID);//Lấy hết các blog
+            if (listApproveBlogs.size() > 0) {
+//                String value = request.getParameter("sortBy");
+//                String orderValue = request.getParameter("sortOrder");
+//                if ("date".equals(value)) {//Nếu người dùng chọn sort by date
+//                    if("descending".equals(orderValue)){//Sắp xếp theo giảm dần
+//                        Collections.sort(listApproveBlogs, BlogDTO.compareDate);
+//                        request.setAttribute("ORDER_OPTION", "Descending");
+//                    }else if("ascending".equals(orderValue)){//Sắp xếp giảm dần
+//                        Collections.sort(listApproveBlogs, BlogDTO.compareDate);
+//                        Collections.reverse(listApproveBlogs);
+//                        request.setAttribute("ORDER_OPTION", "Ascending");
+//                    }else{
+//                        request.setAttribute("ORDER_OPTION", "None");
+//                    }
+//                    request.setAttribute("OPTION", "Date");
+//                } 
+                Collections.sort(listApproveBlogs, BlogDTO.compareDate);
+                request.setAttribute("LIST_ALL_BLOGS", listApproveBlogs);
+            }
+            url = SUCCESS;
+        } catch (Exception e) {
+            log("Error at Get List Controller: " + e.toString());
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
